b07d1492a26c24c025e722e12c8c20af1336e688
anapgh <anapogarciah@gmail.com>
2021-03-04 19:04:56 +0100

    SACA EL NIVEL DE UNA LIST COMPREHENSION

--- ClassIterTree.py
+++ ClassIterTree.py
@@ -1,38 +1,49 @@
 import ast
 import csv
+import niveles
+
 #-- clase para iterar el arbol
 
 class ClassIterTree():
+
+    #-- Constructor de clase
     def __init__(self, tree, atrib, pos):
         self.tree = tree
         self.atrib = atrib
         self.name = pos
+        self.locali_arbol()
 
+
+    #-- Método que itera sobre el árbol
     def locali_arbol(self):
         for self.node in ast.walk(self.tree):
             #-- Buscamos atribs
             if type(self.node) == eval(self.atrib):
+                #-- En el caso de que sea List Comprehension
+                #-- Asignamos la lista de valores
+                niveles.nivel_ListComp(self)
                 self.asignar_lista()
 
 
+    #-- Crear lista del objeto
     def asignar_lista(self):
-        self.nivel = '' # Nivel
-        self.list = [self.name, type(self.node), self.node.lineno,
+        self.list = [self.name, self.clase, self.node.lineno,
                     self.node.end_lineno, self.node.col_offset, self.nivel]
         print(self.list)
         self.add_csv()
 
-    #-- Lista del csv
+    #-- Cabecera del csv
     myData =[['Nombre fichero', 'Clase', 'Linea empiece','Linea acabado',
                 'Desplazamiento', 'Nivel']]
 
+    #-- Añadir la lista del objeto al CSV
     def add_csv(self):
         self.myData.append(self.list)
         print(self.myData)
         self.leer_fichero_csv()
 
 
-    #-- Crear fichero.csv
+    #-- Crear y añadir datos en el fichero.csv
     def leer_fichero_csv(self, fichero_csv = ""):
         if not fichero_csv:
             fichero_csv = open('datos.csv', 'w')
