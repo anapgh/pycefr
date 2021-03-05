af56834590e375d2646d9d56ada18123c868313e
anapgh <anapogarciah@gmail.com>
2021-03-05 09:20:40 +0100

    simplificado, ahora lee todos los elementos

--- prueba4.py
+++ prueba4.py
@@ -2,35 +2,34 @@
 import ast
 import os
 import csv
-from ClassTree import ClassTree
 from ClassIterTree import ClassIterTree
+import ClassElement as ClassE
 
 #-- Creamos listas de cada atrib
 Literals = ['ast.Constant', 'ast.FormattedValue', 'ast.JoinedStr', 'ast.List', 'ast.Tuple', 'ast.Set',
             'ast.Dict']
 
-Variables = ['ast.Name', 'ast.Load', 'ast.Store', 'ast.Del', 'ast.Starred']
-Expressions = ['ast.Expr', 'ast.UnaryOp', 'ast.UAdd', 'ast.USub', 'ast.Not', 'ast.Invert', 'ast.BinOp',
-                'ast.Add', 'ast.Sub', 'ast.Mult', 'ast.Div', 'ast.FloorDiv', 'ast.Mod', 'ast.Pow', 'ast.LShift',
+Variables = ['ast.Name', 'ast.Del', 'ast.Starred']
+Expressions = ['ast.Expr', 'ast.UnaryOp', 'ast.UAdd', 'ast.USub', 'ast.Invert', 'ast.BinOp',
+                'ast.Sub','ast.Div', 'ast.FloorDiv', 'ast.LShift',
                 'ast.RShift', 'ast.BitOr', 'ast.BitXor', 'ast.BitAnd', 'ast.MatMult', 'ast.BoolOp',
-                'ast.And', 'ast.Or', 'ast.Compare', 'ast.Eq', 'ast.NotEq', 'ast.Lt', 'ast.LtE', 'ast.Gt', 'ast.GtE',
-                'ast.Is', 'ast.IsNot', 'ast.In', 'ast.NotIn', 'ast.Call', 'ast.keyword', 'ast.IfExp',
+                'ast.And', 'ast.Compare', 'ast.LtE', 'ast.GtE',
+                'ast.Is', 'ast.IsNot', 'ast.NotIn', 'ast.Call', 'ast.keyword', 'ast.IfExp',
                 'ast.Attribute', 'ast.NamedExpr']
-Subscripting = ['ast.Subscript', 'ast.Slice']
-Comprehensions = ['ast.ListComp', 'ast.SetComp', 'ast.GeneratorExp', 'ast.DictComp',
-                  'ast.comprehension']
-Statements = ['ast.Assig', 'ast.AnnAssign', 'ast.AugAssign', 'ast.Raise', 'ast.Assert', 'ast.Delete',
+Subscripting = ['ast.Subscript']
+Comprehensions = ['ast.ListComp', 'ast.SetComp', 'ast.GeneratorExp', 'ast.DictComp']
+Statements = ['ast.Assign', 'ast.AnnAssign', 'ast.AugAssign', 'ast.Raise', 'ast.Assert', 'ast.Delete',
               'ast.Pass']
-Imports = ['ast.Import', 'ast.ImportFrom', 'ast.alias']
+Imports = ['ast.Import', 'ast.ImportFrom']
 ControlFlow = ['ast.If', 'ast.For', 'ast.While', 'ast.Break', 'ast.Continue', 'ast.Try',
-               'ast.ExceptHandler', 'ast.With', 'ast.Withitem']
-FunctionsClass = ['ast.FunctionDef', 'ast.Lambda', 'ast.arguments', 'ast.arg', 'ast.Return',
+               'ast.ExceptHandler', 'ast.With']
+FunctionsClass = ['ast.FunctionDef', 'ast.Lambda', 'ast.arg', 'ast.Return',
                    'ast.Yield', 'ast.YieldFrom', 'ast.Global', 'ast.Nonlocal', 'ast.ClassDef']
 AsyncAwait = ['ast.AsyncFunctionDef', 'ast.Await', 'ast.AsyncFor', 'ast.AsyncWith']
 
 #-- Creamos lista de listas de atribs
 SetClass = [Literals, Variables, Expressions, Subscripting, Comprehensions,
-          Statements, Imports, ControlFlow, FunctionsClass, AsyncAwait]
+            Statements, Imports, ControlFlow, FunctionsClass, AsyncAwait]
 
 
 #-- Extraemos del directorio los archivos .py
@@ -44,7 +43,6 @@ def leer_directorio():
             print('fichero python: ' + str(directorio[i]))
             pos = directorio[i]
             leer_fichero(directorio[i], pos)
-
 
 
 #-- Leemos el fichero y nos devuelve el arbol
@@ -62,164 +60,10 @@ def iterar_lista(tree, pos):
         for j in range(0, len(SetClass[i])):
             atrib = SetClass[i][j]
             profundizar(tree, atrib, pos)
-"""
-#-- Lista del csv
-myData =[['Nombre fichero', 'Clase', 'Linea empiece','Linea acabado',
-            'Desplazamiento', 'Nivel']]
-
-#-- Crear fichero.csv
-def leer_fichero_csv():
-    myCsv = open('datos.csv', 'w')
-    with myCsv:
-        writer = csv.writer(myCsv)
-        writer.writerows(myData)
-"""
-"""
-#-- LISTAS
-def lista(tree, atrib,pos):
-    nivel = ''
-    clase = ''
-    if atrib == 'ast.List':
-        for node in ast.walk(tree):
-            if type(node) == eval(atrib):
-                print ('LISTA:')
-                #print(ast.literal_eval(node))
-                print(node.elts)
-                for node1 in ast.walk(node):
-                    if type(node1) == eval(atrib):
-                        print('LISTA DE LISTAS')
-                        nivel = 'A2'
-                        clase = str(type(node)) + str(type(node1))
-                    else:
-                        print ('LISTA NORMAL')
-                        nivel = 'A1'
-                        clase = type(node)
-                    #-- Añadir datos en list:
-                    list = [pos, clase, node.lineno, node.end_lineno,
-                                node.col_offset, nivel]
-                    #-- Añadir list en la lista myData que se convierte en .csv
-                    myData.append(list)
-
-
-#-- LIST COMPREHENSION
-def list_comprehension2(tree, atrib, pos):
-    nivel = ''
-    clase = ''
-    if atrib == 'ast.ListComp':
-        for node in ast.walk(tree):
-            numComp = 0
-            if type(node) == eval(atrib):
-                print('LIST COMPREHENSION:')
-                print(node.generators)
-                for i in node.generators:
-                    print (i)
-                    numComp += 1
-                if numComp > 1:
-                    print('ES UNA LIST COMPREHENSION ANIDADA')
-                    nivel = 'C2'
-                    clase = str(numComp) + ' ' + str(type(node))
-                else:
-                    print('ES UNA LIST COMPREHENSION NORMAL')
-                    nivel = 'C1'
-                    clase = type(node)
-                #-- Añadir datos en listComp:
-                listComp = [pos, clase,node.lineno, node.end_lineno,
-                            node.col_offset, nivel]
-                #-- Añadir listComp en la lista myData que se convierte en .csv
-                myData.append(listComp)
-
-
--- DICCIONARIOS
-def diccionario(tree, atrib, pos):
-    nivel = ''
-    if atrib == 'ast.Dict':
-        for node in ast.walk(tree):
-            #-- Buscamos atribs
-            if type(node) == eval(atrib):
-                print('DICCIONARIO:')
-                d1 = ast.literal_eval(node)
-                print(d1)
-                nivel = 'A1'
-                clase = type(node)
-                #-- Añadir datos en dicc:
-                #-- Crear instancia de objeto
-                D = ClassTree(pos, clase, node.lineno, node.end_lineno,node.col_offset, nivel)
-                D.add_csv()
-                D.leer_fichero_csv()
-                for node1 in ast.walk(node):
-                    atrib1 = ast.List
-                    #NO FUNCIONA BIEN:
-                    if type(node1) == atrib:
-                        print('DICCIONARIO DENTRO DE DICCIONARIO')
-                        print(ast.literal_eval(node1))
-                        print (str(atrib1) + ':')
-                        print (node1.lineno) #-- Primera linea del texto
-                        print (node1.end_lineno) #-- Ultima linea del texto
-                        print('elementos:')
-                        print(node1.values)
-                    ##################
-
-                    if type(node1) == atrib1:
-                        print('DICCIONARIO DE LISTAS:')
-                        print(ast.literal_eval(node1))
-                        nivel = 'A2'
-                        clase = str(type(node)) + str(type(node1))
-                        #-- Añadir datos en dicc:
-                        D = ClassTree(pos, clase, node1.lineno, node1.end_lineno,
-                                        node1.col_offset, nivel)
-                        #-- Añadir dicc en la lista myData que se convierte en .csv
-                        D.add_csv()
-                        D.leer_fichero_csv()
-
-
-#-- DICT COMPREHENSION
-def dict_comprehension(tree, atrib, pos):
-    nivel = ''
-    clase = ''
-    if atrib == 'ast.DictComp':
-        for node in ast.walk(tree):
-            numComp = 0
-            numIfs = 0
-            if type(node) == eval(atrib):
-                print('DICT COMPREHENSION:')
-                for i in node.generators:
-                    print(i.ifs)
-                    for j in i.ifs:
-                        numIfs += 1
-                        numComp += 1
-                        if numIfs > 1:
-                            print('ES UN DICT COMPREHENSION CONDICIONAL')
-                            nivel = 'C2'
-                            clase = str(numIfs) + 'IFS +  ' + str(type(node))
-                        if numComp > 1:
-                            print('ES UN DICT COMPREHENSION ANIDADO')
-                            nivel = 'C2'
-                            clase = str(numComp) + ' ' + str(type(node))
-                        else:
-                            print('ES UN DICT COMPREHENSION')
-                            nivel = 'C1'
-                            clase = type(node)
-                        #-- Añadir datos en dictComp:
-                        dictComp = [pos, clase,node.lineno, node.end_lineno,
-                                    node.col_offset, nivel]
-                        #-- Añadir dictComp en la lista myData que se convierte en .csv
-                        myData.append(dictComp)
-
-"""
-#-- LIST COMPREHENSION
-def list_comprehension(tree, atrib, pos):
-    if atrib == 'ast.ListComp':
-        LC = ClassIterTree(tree, atrib, pos)
-        LC.locali_arbol()
-
 
 
 def profundizar(tree, atrib, pos):
-    #lista(tree, atrib,pos)
-    list_comprehension(tree, atrib, pos)
-    #diccionario(tree, atrib, pos)
-    #dict_comprehension(tree, atrib, pos)
-
+    objeto = ClassIterTree(tree, atrib, pos)
 
 
 if __name__ == "__main__":
