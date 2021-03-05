aa2cfc39d051a842e8a89b5c2da1508132a9a14f
anapgh <anapogarciah@gmail.com>
2021-03-05 10:37:31 +0100

    añadido nivel de dictComp

--- niveles.py
+++ niveles.py
@@ -1,12 +1,31 @@
-import ast
+#-- PROGRAMA PARA LOS NIVELES DE CADA ATRIBUTO
+
 #-- NIVELES
 def niveles(self):
-    if self.atrib == 'ast.ListComp':
+    if self.atrib == 'ast.List':
+        nivel_List(self)
+    elif self.atrib == 'ast.ListComp':
         nivel_ListComp(self)
     elif self.atrib == 'ast.Dict':
         nivel_Dict(self)
-    elif self.atrib == 'ast.List':
-        nivel_List(self)
+    elif self.atrib == 'ast.DictComp':
+        nivel_DictComp(self)
+
+
+#-- NIVEL DE LISTAS
+def nivel_List(self):
+    num_List = 0
+    num_Dict = 0
+    #-- Comprobamos si hay listas
+    if 'ast.List' in str(self.node.elts):
+        print('hay lista')
+        numList = str(self.node.elts).count('ast.List')
+        self.nivel = 'A2'
+        self.clase = str(numList) + ' Listas en' + str(type(self.node))
+    else:
+        print('no hay nada')
+        self.nivel = 'A1'
+        self.clase = type(self.node)
 
 
 #-- NIVEL LIST COMPREHENSION
@@ -55,17 +74,33 @@ def nivel_Dict(self):
         self.nivel = 'A2'
         self.clase = type(self.node)
 
-#-- NIVEL DE LISTAS
-def nivel_List(self):
-    num_List = 0
-    num_Dict = 0
-    #-- Comprobamos si hay listas
-    if 'ast.List' in str(self.node.elts):
-        print('hay lista')
-        numList = str(self.node.elts).count('ast.List')
-        self.nivel = 'A2'
-        self.clase = str(numList) + ' Listas en' + str(type(self.node))
-    else:
-        print('no hay nada')
-        self.nivel = 'A1'
-        self.clase = type(self.node)
+def nivel_DictComp(self):
+    numIfs = 0
+    ifExp = 0
+    numDictComp = 0
+    print('DICT COMP:')
+    print(self.node.generators)
+    print('VALUE')
+    print(self.node.value)
+    for i in self.node.generators:
+        print('IFSSSSSSSS')
+        print(i.ifs)
+        numIfs += str(i.ifs).count('ast.Compare')
+        if numIfs > 0:
+            print('DICT COMPREHENSION CONDICIONAL')
+            self.nivel = 'C2'
+            self.clase = (str(numIfs) + ' Ifs en: ' + str(type(self.node)))
+        else:
+            print('DICT COMPREHENSION NORMAL')
+            self.nivel = 'C1'
+            self.clase = type(self.node)
+    if 'ast.IfExp' in str(self.node.value):
+        ifExp += str(self.node.value).count('ast.IfExp')
+        print('DICT COMPREHENSION IF - ELSE')
+        self.nivel = 'C2'
+        self.clase = (str(ifExp) + ' IF - ELSE en : ' + str(type(self.node)))
+    elif 'ast.DictComp' in str(self.node.value):
+        numDictComp += str(self.node.value).count('ast.DictComp')
+        print('DICT COMPREHENSION ANIDADA')
+        self.nivel = 'C2'
+        self.clase = (str(numDictComp) + str(type(self.node)))
