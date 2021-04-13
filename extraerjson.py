a59cf44b4af54bc4c5c6ecf36682b1f8549f474b
anapgh <anapogarciah@gmail.com>
2021-04-13 22:37:19 +0200

    Todo en ingles

--- extraerjson.py
+++ extraerjson.py
@@ -2,55 +2,56 @@
 
 import json
 
-listNiveles = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+listLevels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
 
-def extraer_niveles(data):
-    #-- Sacamos los ficheros que hay (keys)
-    for fich in data.keys():
-        #-- Inicilizo los niveles a 0
+#-- Extract directory levels
+def extract_Levels(data):
+    #-- Take out the files (keys)
+    for file in data.keys():
+        #-- Initialise levels to 0
         A1 = 0
         A2 = 0
         B1 = 0
         B2 = 0
         C1 = 0
         C2 = 0
-        for i in data[fich]:
-            nivel = (i["Nivel"])
-            #-- Contamos cada nivel en cada fichero
-            for valor in listNiveles:
-                if valor in nivel:
-                    if valor == 'A1':
-                        A1 += nivel.count(valor)
-                    elif valor == 'A2':
-                        A2 += nivel.count(valor)
-                    elif valor == 'B1':
-                        B1 += nivel.count(valor)
-                    elif valor == 'B2':
-                        B2 += nivel.count(valor)
-                    elif valor == 'C1':
-                        C1 += nivel.count(valor)
-                    elif valor == 'C2':
-                        C2 += nivel.count(valor)
+        for i in data[file]:
+            level = (i["Level"])
+            #-- Count each level in each file
+            for value in listLevels:
+                if value in level:
+                    if value == 'A1':
+                        A1 += level.count(value)
+                    elif value == 'A2':
+                        A2 += level.count(value)
+                    elif value == 'B1':
+                        B1 += level.count(value)
+                    elif value == 'B2':
+                        B2 += level.count(value)
+                    elif value == 'C1':
+                        C1 += level.count(value)
+                    elif value == 'C2':
+                        C2 += level.count(value)
 
-        #-- Guardamos resultados en myDataNivel
-        myDataNivel = ('Fichero: ' + fich + '\n' + 'Niveles: ' + '\n' +
+        #-- Save results in myDataLevel
+        myDataLevel = ('File: ' + file + '\n' + 'Levels: ' + '\n' +
                         'A1: ' + str(A1) + '\n' + 'A2: ' + str(A2) + '\n' +
                         'B1: ' + str(B1) + '\n' + 'B2: ' + str(B2) + '\n'+
                         'C1: ' + str(C1) + '\n' + 'C2: ' + str(C2) + '\n\n')
-        escribir_resultados(myDataNivel)
+        write_Results(myDataLevel)
 
-#-- Creamos un archivo.txt con un resumen de resultados
-def escribir_resultados(myDataNivel):
-    with open('resumen.txt', 'a') as file:
-        file.write(myDataNivel)
+#-- Create a .txt file with a summary of results
+def write_Results(myDataLevel):
+    with open('summary.txt', 'a') as file:
+        file.write(myDataLevel)
         file.close()
     print('Fichero creado...')
 
-def leer_json():
+def read_Json():
     with open('data.json') as file:
         data = json.load(file)
-        extraer_niveles(data)
+        extract_Levels(data)
 
 
 if __name__ == "__main__":
-    leer_json()
+    read_Json()
