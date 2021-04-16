170d551045e34a2d9d8797e1d3d6a217efd5da94
anapgh <anapogarciah@gmail.com>
2021-04-16 23:08:59 +0200

    niveles y clases

--- getjson.py
+++ getjson.py
@@ -3,8 +3,6 @@ import json
 
 dict_total = {}
 
-
-
 #-- Extract repository levels
 def extract_Levels(data):
     #-- Take out the repositories
@@ -17,47 +15,26 @@ def extract_Levels(data):
                 if not 'Levels' in dict_total[repo][file]:
                     #-- Initialize the dictionary values to 0
                     #-- Create the 'Levels' key
-                    dict_total[repo][file]['Levels'] = ini_values('Levels')
-                get_level(level, repo, file)
+                    dict_total[repo][file]['Levels'] = {}
+                ini_values(repo, file, 'Levels', level)
+                #get_level(level, repo, file)
                 clase = i['Class']
                 if not 'Class' in dict_total[repo][file]:
                     #-- Initialize the dictionary values to 0
                     #-- Create the 'Class' key
                     dict_total[repo][file]['Class'] = {}
+                ini_values(repo, file, 'Class', clase)
 
         write_Results(repo)
     #print(dict_total)
 
 
-def ini_values(type):
-    dict_aux = dict()
-    list_levels = []
-    if type == 'Levels':
-        list_levels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
-
+def ini_values(repo, file, type, key):
+    if not key in dict_total[repo][file][type]:
+        if key != "":
+            dict_total[repo][file][type][key] = 1
     else:
-        list_levels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
-    #-- Initialize the dictionary values to 0
-    for k in list_levels:
-        dict_aux[k] = 0
-
-    return dict_aux
-def get_level(level, repo, file):
-    list_levels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
-    for value in list_levels:
-        if value == level:
-            dict_total[repo][file]['Levels'][value] += 1
-
-
-def get_class(clase, repo, file):
-    class_list(clase, repo, file)
-
-
-def class_list(clase, repo, file):
-    list_list = ['Simple List', 'Nested List', 'Dictionary List']
-    for value in list_list:
-        if value in str(clase):
-            dict_total[repo][file]['Class'][value] += 1
+        dict_total[repo][file][type][key] += 1
 
 
 #-- Create a .txt file with a summary of results
