4207e63264ba2d01d346311eb36918d7d4c5e91b
anapgh <anapogarciah@gmail.com>
2021-04-23 18:46:29 +0200

    resumen de cada repo

--- WEB/main.js
+++ WEB/main.js
@@ -9,11 +9,13 @@ let INDEX = fs.readFileSync('main.html', 'utf-8')
 //-- Name of the Json file to read
 const JSON_FILE = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/total_data.json")
 const JSON_FILESUM = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/summary_data.json")
+const JSON_FILEREPO = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/repo_data.json")
 
 //-- Create the store structure from the contents of the file
 //-- Return us the json structure
 var data = JSON.parse(JSON_FILE);
 var summary = JSON.parse(JSON_FILESUM)
+var data_repo = JSON.parse(JSON_FILEREPO)
 
 //-- Variable that is going to have all the buttons available
 let button = ''
@@ -38,7 +40,7 @@ for (repo=0; repo<repository.length; repo++){
   let content = REPO
   //-- Variable total
   let total = ''
-  total += "<h2> REPOSITORY: " + repository[repo] + "</h2>" + '\n'
+  let name_repo = "<h2> REPOSITORY: " + repository[repo] + "</h2>" + '\n'
   //-- Get total content
   content_total = data[repository[repo]]
 
@@ -68,11 +70,45 @@ for (repo=0; repo<repository.length; repo++){
       total += ('<p>Class ' + keys[i] + ': ' + values[i] + '</p>' + '\n')
     }
   }
+  total_repo = repo_summary()
+  content = content.replace('REPO', name_repo)
   content = content.replace('TOTAL', total)
+  content = content.replace('SUMMARY', total_repo)
+
   name_html = (repository[repo] + '.html')
+
 
   //-- Write total in new html file
   fs.writeFileSync(name_html, content);
+}
+
+function repo_summary(){
+  //-- Obtain repository summary information
+  repos = Object.keys(data_repo)
+  //console.log(repo)
+  for(i=0; i<repos.length;i++){
+    let total_repo = '';
+    repo_name = repos[i]
+    content = data_repo[repo_name]
+
+
+    for(elem=0; elem<Object.keys(content).length; elem++){
+      keys = Object.keys(content);
+      values = Object.values(content);
+      if (elem==0){
+        total_repo += ('<h4>LEVELS: <h4>' + '\n');
+      }else{
+        total_repo += ('<h4>CLASSES: <h4>' + '\n');
+      }
+      for (value=0; value<Object.keys(values[elem]).length; value++){
+        key = Object.keys(values[elem])[value];
+        val =Object.values(values[elem])[value];
+        total_repo += ('<p>' + keys[elem] + ' ' + key + ': ' + val +
+                       '</p>' + '\n');
+      }
+    }
+    return(total_repo)
+  }
 }
 
 //-- Obtain summary information
