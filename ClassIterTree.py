4151beb41067b995d8c61a080c74c1d2d8a822e4
Gregorio <grex@gsyc.urjc.es>
2021-06-10 18:34:31 +0200

    Docstrings corrected

--- ClassIterTree.py
+++ ClassIterTree.py
@@ -5,20 +5,27 @@ import csv
 import json
 import levels
 
-""" Class to iterate tree. """
-class IterTree():
 
-    """ Class constructor. """
+class IterTree():
+    """ Class to iterate tree. """
+
+    #-- CSV header
+    myDataCsv =[['Repository', 'File Name', 'Class', 'Start Line','End Line',
+                'Displacement', 'Level']]
+
+    # JSON dictionary
+    myDataJson = {}
+    
     def __init__(self, tree, attrib, file, repo):
+        """ Class constructor. """
         self.tree = tree
         self.attrib = attrib
         self.name = file
         self.repo = repo
         self.locate_Tree()
 
-
-    """ Method iterating on the tree. """
     def locate_Tree(self):
+        """ Method iterating on the tree. """
         for self.node in ast.walk(self.tree):
             #-- Find attributes
             if type(self.node) == eval(self.attrib):
@@ -30,29 +37,22 @@ class IterTree():
                 self.read_FileJson()
 
 
-    """ Create object list. """
     def assign_List(self):
+        """ Create object list. """
         if (self.clase != '') and (self.level != ''):
             self.list = [self.repo, self.name, self.clase, self.node.lineno,
                         self.node.end_lineno, self.node.col_offset, self.level]
             #print(self.list)
             self.add_Csv()
 
-
-    #-- Csv header
-    myDataCsv =[['Repository', 'File Name', 'Class', 'Start Line','End Line',
-                'Displacement', 'Level']]
-
-
-    """ Add object list to CSV. """
     def add_Csv(self):
+        """ Add object list to CSV. """
         self.myDataCsv.append(self.list)
         #print(self.myDataList)
         self.read_FileCsv()
 
-
-    """ Create and add data in the .csv file. """
     def read_FileCsv(self, file_csv = ""):
+        """ Create and add data in the .csv file. """
         if not file_csv:
             file_csv = open('data.csv', 'w')
             with file_csv:
@@ -63,13 +63,8 @@ class IterTree():
                 writer = csv.writer(f)
                 writer.writerow(self.myDataCsv)
 
-
-    #-- Create json dictionary
-    myDataJson = {}
-
-
-    """ Create object dictionary. """
     def assign_Dict(self):
+        """ Create object dictionary. """
         if (self.clase != '') and (self.level != ''):
             if not self.repo in self.myDataJson:
                 self.myDataJson[self.repo] = {}
@@ -84,8 +79,7 @@ class IterTree():
                 'Displacement': str(self.node.col_offset),
                 'Level'       : str(self.level)})
 
-
-    """ Create and add data in the .json file. """
     def read_FileJson(self):
+        """ Create and add data in the .json file. """
         with open('data.json', 'w') as file:
             json.dump(self.myDataJson, file, indent=4)
